2aec2eda9ceb3986486610a835164f6873bc85b1
Shaifali Kalia <shaifalikalia@gmail.com>
2024-12-25 21:54:03 +0530

    updated the node version and fix the build issues

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import './Projects.css'
 import { Fade } from 'react-reveal'
-import { featured_projects } from '../../data/featured_projects.json'
+import projectsData from '../../data/featured_projects.json'
 import Project from '../project/Project'
 import Section from '../section/Section'
 
 const Projects = () => {
-
+  const { featured_projects } = projectsData
   return (
     <Section title="Projects">
-        <div className="projects-content">
-          <ul className="projects-list">
-            {featured_projects.map((project) => {
-              return (
-                <li key={project.name}>
-                  <Fade bottom duration={1000} distance="20px">
-                    <Project project={project}/>
-                  </Fade>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      </Section>
+      <div className="projects-content">
+        <ul className="projects-list">
+          {featured_projects.map((project) => {
+            return (
+              <li key={project.name}>
+                <Fade bottom duration={1000} distance="20px">
+                  <Project project={project} />
+                </Fade>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </Section>
   )
 }
 
